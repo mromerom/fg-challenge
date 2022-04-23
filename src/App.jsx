192c59289b705414a6430b167c055d57b13f2d0e
Matthew <matt.romero@me.com>
2022-04-23 14:14:01 -0500

    feature(Form): Create basic shape

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import './App.css';
+import { Form } from './components/form'
+
+export const App = () => {
+  return (
+    <Form />
+  )
+}
---
+++ src/components/button.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Button = (props) => {
+  const {
+    children
+  } = props
+
+  return <button>{children}</button>
+}
