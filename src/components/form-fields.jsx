98d6b7dcd2bbd40b03df3c182a31d0ceb5603b6b
Matthew <matt.romero@me.com>
2022-04-25 20:28:25 -0500

    feature(Logic): [bugged] values submit, trouble on callback

--- src/components/form-fields.jsx
+++ src/components/form-fields.jsx
@@ -4,9 +4,9 @@ import { Button } from './button'
 import { useInput } from '../utils/useInput'
 import { validateFields } from '../utils/validation'
 
-export const FormFields = (props) => {
-  const {} = props
-  const { errors, handleSubmit } = useInput(validateFields)
+export const FormFields = ({submit}) => {
+  // const { submit } = props
+  const { errors, handleChange, handleSubmit, values } = useInput(submit, validateFields)
 
   return (
     <div>
@@ -22,24 +22,59 @@ export const FormFields = (props) => {
           </p>
         </div>
         {errors.firstName && <p className='form--error'>{errors.firstName}</p>}
-        <Input
-          field='firstName'
-          labelText='First Name'
-          type='text'
-        />
+        <div className='composed-input'>
+          <label className={`input--label`} htmlFor='firstName'>
+            First Name
+          </label>
+          <input
+            id='firstName'
+            name='firstName'
+            type='text'
+            value={values.firstName}
+            onChange={handleChange}
+          />
+        </div>
         {errors.email && <p className='form--error'>{errors.email}</p>}
-        <Input
+        {/* <Input
           field='email'
           labelText='Email Address'
           type='email'
-        />
+          value={values.email}
+          onChange={handleChange}
+        /> */}
+        <div className='composed-input'>
+          <label className={`input--label`} htmlFor='email'>
+            Email Address
+          </label>
+          <input
+            id='email'
+            name='email'
+            type='email'
+            value={values.email}
+            onChange={handleChange}
+          />
+        </div>
         {errors.password && <p className='form--error'>{errors.password}</p>}
         <Input
           field='password'
           labelText='Password'
           type='password'
+          value={values.password}
+          onChange={handleChange}
         />
-        <Button onSubmit={handleSubmit}>Sign Up</Button>
+        {/* <div className='composed-input'>
+          <label className={`input--label`} htmlFor='password'>
+            Password
+          </label>
+          <input
+            id='password'
+            name='password'
+            type='password'
+            value={values.password}
+            onChange={handleChange}
+          />
+        </div> */}
+        <Button>Sign Up</Button>
       </form>
     </div>
   )
--- src/utils/validation.js
+++ src/utils/validation.js
@@ -6,11 +6,13 @@ export const validateName = (values, errors) => {
   // Chloë
   const regex = /([A-zÀ-ž]+(?:[ '-][A-zÀ-ž]+){1,25})/g
 
-    if (values.firstName.trim()) {
+    if (!values.firstName.trim()) {
       errors.firstName = 'Please provide your first name.'
-    } else if (!regex.test(values.firstName)) {
+    } else if (regex.test(!values.firstName)) {
       errors.firstName = 'First names can only contain letters, hyphens, apostrophes, spaces, and 25 characters total.'
     }
+
+    // return errors
 }
 
 export const validateEmail = (values, errors) => {
@@ -21,9 +23,11 @@ export const validateEmail = (values, errors) => {
 
   if (!values.email) {
     errors.email = 'Please provide an email address.'
-  } else if (!regex.test(values.email)) {
+  } else if (regex.test(!values.email)) {
     errors.email = 'Please provide a valid email address.'
   }
+
+  // return errors
 }
 
 export const validatePassword = (values, errors) => {
@@ -32,13 +36,10 @@ export const validatePassword = (values, errors) => {
   if (!values.password) {
     errors.password = 'Please create a password.'
   } else if (!regex.test(values.password)) {
-    errors.password = `Password must contain at least:\n
-      \n• eight characters (and 64 max)
-      \n• one uppercase letter
-      \n• one lowercase letter
-      \n• one digit
-      \n• and one special character.`
+    errors.password = 'Password must contain at least eight characters (and 64 max), one uppercase letter, one lowercase letter, one digit, and one special character.'
   }
+
+  // return errors
 }
 
 export const validateFields = (values) => {
@@ -47,6 +48,33 @@ export const validateFields = (values) => {
   validateName(values, errors)
   validateEmail(values, errors)
   validatePassword(values, errors)
+
+  // const nameRegex = /([A-zÀ-ž]+(?:[ '-][A-zÀ-ž]+){1,25})/
+
+  // if (!values.firstName.trim()) {
+  //   errors.firstName = 'Please provide your first name.'
+  // }
+  // else if (nameRegex.test(!values.firstName)) {
+  //   errors.firstName = 'First names can only contain letters, hyphens, apostrophes, spaces, and 25 characters total.'
+  // }
+
+  // const emailRegex = /^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/
+
+
+  // if (!values.email) {
+  //   errors.email = 'Please provide an email address.'
+  // } else if (!emailRegex.test(values.email)) {
+  //   errors.email = 'Please provide a valid email address.'
+  // }
+
+  // const pwdRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,64}$/i
+
+  // if (!values.password) {
+  //   errors.password = 'Please create a password.'
+  // } 
+  // else if (pwdRegex.test(!values.password)) {
+  //   errors.password = `Password must contain at least eight characters (and 64 max), one uppercase letter, one lowercase letter, one digit, and one special character.`
+  // }
   
   return errors
 }
