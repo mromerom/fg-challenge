600a83de1a2832ecd602edc940dd7b9e453fc2ab
Matthew <matt.romero@me.com>
2022-04-26 23:01:38 -0500

    chore(Logic): Clean up code

--- src/components/form-success.jsx
+++ src/components/form-success.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { Button } from './button'
 import { SignupContext } from '../contexts/signup-context'
 
-export const FormSuccess = (props) => {
+export const FormSuccess = () => {
   const { email, firstName } = React.useContext(SignupContext)
 
   return (
