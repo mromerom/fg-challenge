412d2d555b5595479855ff0b5f8fff636944293e
Matthew Romero Moore <12155283+mromerom@users.noreply.github.com>
2022-04-26 23:02:27 -0500

    chore(Logic): Clean up code (#24)

--- src/components/form.jsx
+++ src/components/form.jsx
@@ -8,38 +8,19 @@ export const Form = () => {
   const [firstName, setFirstName] = React.useState('')
   const [email, setEmail] = React.useState('')
   const [password, setPassword] = React.useState('')
-  const [errors, setErrors] = React.useState({})
   const [submission, setSubmission] = React.useState(false)
-  const [values, setValues] = React.useState({
-    email: '',
-    firstName: '',
-    password: '',
-  })
 
   const handleSubmit = event => {
     event.preventDefault()
     setSubmission(true)
   }
 
-
-  React.useEffect((e) => {
-    // console.log(errors)
-    if (Object.keys(errors).length === 0 && submission) {
-      // e.preventDefault()
-      // setErrors(validateFields(firstName, email, password))
-      setSubmission(true)
-    }
-  }, [errors, submission])
-
   return (
       <SignupContext.Provider value={{
         email, setEmail,
-        errors, setErrors,
         firstName, setFirstName,
         password, setPassword,
         submission, setSubmission,
-        values, setValues,
-        // handleChange,
         handleSubmit,
       }}>
         {!submission ? (
